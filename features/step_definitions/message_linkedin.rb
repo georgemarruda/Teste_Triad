1bb67c429511107487458e34f4548c839868ffec
Georgem <georgemarruda@grupofortes.com.br>
2017-05-26 23:45:55 -0300

    'teste'

---
+++ features/step_definitions/message_linkedin.rb
@@ -0,0 +1,11 @@
+Given(/^I am logged in on linkedin$/) do
+    
+end
+
+When(/^I search for a user$/) do
+  #XXXXXXXXXX
+end
+
+Then(/^I must write the message and send it$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
